c1993cea6c1cb723c819fffed2f2df179ebc907d
MAGICAL-KX <xkenton0@gmail.com>
2024-09-01 11:22:52 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "263789432940"
+global.ownername = "magical kx👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
-global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '263789432940'  //creator number
+global.ownername = 'MAGICAL KX👑' //owner name
+global.botname = 'KENO BUGS ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +263717750825'
+global.author = 'KENO BUGS👑\n\nContact: +263717750825'
 
 //console view/theme
 global.themeemoji = '🪀'
